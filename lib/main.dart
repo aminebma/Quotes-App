1550deae34f456b8ca1ee0cc343f8fdd44707af3
Mohamed Amine BENBAKHTA <gm_benbakhta@esi.dz>
2020-01-17 19:30:57 +0100

    First commit, displaying some quotes

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      home: QuoteList(),
+    ));
+
+class QuoteList extends StatefulWidget {
+  @override
+  _QuoteListState createState() => _QuoteListState();
+}
+
+class _QuoteListState extends State<QuoteList> {
+  List<String> quotes = [
+    'Be yourself',
+    'Always aim the moon, even if you miss, you\'ll reach the stars',
+    'Knowledge is Experience'
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text('Awesome Quotes'),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Column(
+          children: quotes.map((quote) => Text(quote)).toList()),
+    );
+  }
+}
