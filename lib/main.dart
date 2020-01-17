26264f28570c9288a63e9b9356b613fe1160bd34
Mohamed Amine BENBAKHTA <gm_benbakhta@esi.dz>
2020-01-17 22:20:05 +0100

    Organising the output into quote cards

--- lib/main.dart
+++ lib/main.dart
@@ -13,22 +13,46 @@ class QuoteList extends StatefulWidget {
 
 class _QuoteListState extends State<QuoteList> {
   List<Quote> quotes = [
-    Quote(text:'Be yourself', author:'Amine BENBAKHTA'),
-    Quote(author:'Mouloud Koudil',text:'Always aim the moon, even if you miss, you\'ll reach the stars'),
-    Quote(author: 'Albert Einstein',text:'Knowledge is Experience')
+    Quote(text: 'Be yourself', author: 'Amine BENBAKHTA'),
+    Quote(
+        author: 'Mouloud Koudil',
+        text: 'Always aim the moon, even if you miss, you\'ll reach the stars'),
+    Quote(author: 'Albert Einstein', text: 'Knowledge is Experience')
   ];
+
+  Widget quoteTemplate(quote) {
+    return Card(
+      margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0.0),
+      child: Column(
+        children: <Widget>[
+          Text(
+            quote.text,
+            style: TextStyle(fontSize: 18.0, color: Colors.grey[600]),
+          ),
+          SizedBox(height: 6.0),
+          Text(
+            quote.author,
+            style: TextStyle(fontSize: 14.0, color: Colors.grey[800]),
+          )
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[200],
-      appBar: AppBar(
-        title: Text('Awesome Quotes'),
-        centerTitle: true,
-        backgroundColor: Colors.redAccent,
-      ),
-      body: Column(
-          children: quotes.map((quote) => Text('${quote.text} - ${quote.author}')).toList()),
-    );
+        backgroundColor: Colors.grey[200],
+        appBar: AppBar(
+          title: Text('Awesome Quotes'),
+          centerTitle: true,
+          backgroundColor: Colors.redAccent,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: quotes.map((quote) => quoteTemplate(quote)).toList()),
+        ));
   }
 }
